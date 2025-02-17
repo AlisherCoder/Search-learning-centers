d638feeb0ebc0aa8f7dae5e3f17f104f16399134
unknown <ibrahimovkamronbek7@.com>
2025-02-17 12:39:53 +0500

    changing

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
    host: "localhost",
    username: "root",
-   password: "953901313",
+   password: "Kamronbek196769*",
    database: "fullstackexam",
    dialect: "mysql",
    logging: false,
