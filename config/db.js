cacd735ecd73909bac097f57746ea10fba8c05d2
Alisher Sharipov <alishersharipov670@gmail.com>
2025-02-17 12:46:57 +0500

    alisher

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
    host: "localhost",
    username: "root",
-   password: "953901313",
+   password: "95490",
    database: "fullstackexam",
    dialect: "mysql",
    logging: false,
